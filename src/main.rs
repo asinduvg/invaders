cb050d387c1a8528297195bd1203adc57399b814
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-08-16 07:23:03 +0530

    The player

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use crossterm::{
     ExecutableCommand,
 };
 use invaders::{
-    frame::{self, new_frame},
-    render,
+    frame::{self, new_frame, Drawable},
+    render, player::Player,
 };
 use rusty_audio::Audio;
 
@@ -45,14 +45,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // Game Loop
+    let mut player = Player::new();
     'gameloop: loop {
         // Per-frame init
-        let curr_frame = new_frame();
+        let mut curr_frame = new_frame();
 
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -63,6 +66,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
 
         // Draw & render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
---
+++ src/player.rs
@@ -0,0 +1,31 @@
+use crate::{NUM_COLS, NUM_ROWS, frame::Drawable};
+
+pub struct Player {
+    x: usize,
+    y: usize
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self {
+            x: NUM_COLS / 2,
+            y: NUM_ROWS - 1
+        }
+    }
+    pub fn move_left(&mut self) {
+        if self.x > 0 {
+            self.x -= 1;
+        }
+    }
+    pub fn move_right(&mut self) {
+        if self.x < NUM_COLS - 1 {
+            self.x += 1;
+        }
+    }
+}
+
+impl Drawable for Player {
+    fn draw(&self, frame: &mut crate::frame::Frame) {
+        frame[self.x][self.y] = "A";
+    }
+}
