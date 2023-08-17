9dfb27fb117837364503c4c292f525feb0585266
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-08-17 07:25:22 +0530

    Invaders

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod frame;
+pub mod invaders;
 pub mod render;
 pub mod player;
 pub mod shot;
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use crossterm::{
 };
 use invaders::{
     frame::{self, new_frame, Drawable},
-    render, player::Player,
+    render, player::Player, invaders::Invaders,
 };
 use rusty_audio::Audio;
 
@@ -47,6 +47,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Game Loop
     let mut player = Player::new();
     let mut instant = Instant::now();
+    let mut invaders = Invaders::new();
     'gameloop: loop {
         // Per-frame init
         let delta = instant.elapsed();
@@ -75,9 +76,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // Updates
         player.update(delta);
+        if invaders.update(delta) {
+            audio.play("move");
+        }
 
         // Draw & render
-        player.draw(&mut curr_frame);
+        let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
+        for drawable in drawables {
+            drawable.draw(&mut curr_frame);
+        }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
